c3cf839fabf89c3a7310890d9cedba45a52759a0
Juan <cesar180687@hotmail.com>
2026-05-26 08:23:46 -0600

    Actvidad division_v2

--- src/ux.curso.programacion/Unidad5/pyhton_division.py
+++ src/ux.curso.programacion/Unidad5/pyhton_division.py
@@ -1,5 +1,13 @@
-a = int(input())
-b = int(input())
+def division(a, b):
+    print(a // b)
+    print(a / b)
 
-print(a // b)
-print(a / b)
+
+def main():
+    num1 = int(input("Ingresa el primer número: "))
+    num2 = int(input("Ingresa el segundo número: "))
+    division(num1, num2)
+
+
+if __name__ == "__main__":
+    main()
