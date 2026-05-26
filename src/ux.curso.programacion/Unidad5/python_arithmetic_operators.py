9284ddd1fd1a6060d05f2c1e90fadbd79ce01126
Juan <cesar180687@hotmail.com>
2026-05-26 08:24:48 -0600

    Actvidad arithmetic operators_v2

--- src/ux.curso.programacion/Unidad5/python_arithmetic_operators.py
+++ src/ux.curso.programacion/Unidad5/python_arithmetic_operators.py
@@ -1,6 +1,14 @@
-a = int(input())
-b = int(input())
+def operaciones(a, b):
+    print(a + b)
+    print(a - b)
+    print(a * b)
 
-print(a + b)
-print(a - b)
-print(a * b)
+
+def main():
+    num1 = int(input("Ingresa el primer número: "))
+    num2 = int(input("Ingresa el segundo número: "))
+    operaciones(num1, num2)
+
+
+if __name__ == "__main__":
+    main()
