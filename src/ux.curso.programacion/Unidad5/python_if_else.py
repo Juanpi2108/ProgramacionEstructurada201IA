85a1a9ef3598b620a102e40ca36ef6ca8cfa97e7
Juan <cesar180687@hotmail.com>
2026-05-26 08:22:13 -0600

    Actvidad if-else_v2

--- src/ux.curso.programacion/Unidad5/python_if_else.py
+++ src/ux.curso.programacion/Unidad5/python_if_else.py
@@ -1,10 +1,18 @@
-n = int(input())
+def verificar_numero(n):
+    if n % 2 != 0:
+        print("Weird")
+    elif n % 2 == 0 and 2 <= n <= 5:
+        print("Not Weird")
+    elif n % 2 == 0 and 6 <= n <= 20:
+        print("Weird")
+    else:
+        print("Not Weird")
 
-if n % 2 != 0:
-    print("Weird")
-elif n % 2 == 0 and 2 <= n <= 5:
-    print("Not Weird")
-elif n % 2 == 0 and 6 <= n <= 20:
-    print("Weird")
-else:
-    print("Not Weird")
+
+def main():
+    numero = int(input("Ingresa un número: "))
+    verificar_numero(numero)
+
+
+if __name__ == "__main__":
+    main()
